a23774a8d47725656cadf25126e0e64f2b79a3dc
Khánh <leqkhanh0109@gmail.com>
2024-11-19 01:43:10 +0700

    update dang nhap, dang ky, dang xuat

--- THBaoMat/DangKy.Designer.cs
+++ THBaoMat/DangKy.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace THBaoMat
 {
-    partial class Dangnhap
+    partial class DangKy
     {
         /// <summary>
         /// Required designer variable.
@@ -28,80 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_login = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
             this.txt_Password = new System.Windows.Forms.TextBox();
             this.txt_Username = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_login
+            // btnRegister
             // 
-            this.btn_login.Location = new System.Drawing.Point(274, 273);
-            this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(116, 34);
-            this.btn_login.TabIndex = 9;
-            this.btn_login.Text = "Đăng nhập";
-            this.btn_login.UseVisualStyleBackColor = true;
-            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            this.btnRegister.Location = new System.Drawing.Point(335, 304);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(116, 34);
+            this.btnRegister.TabIndex = 15;
+            this.btnRegister.Text = "Đăng kí";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // txt_Password
             // 
-            this.txt_Password.Location = new System.Drawing.Point(218, 152);
+            this.txt_Password.Location = new System.Drawing.Point(276, 149);
             this.txt_Password.Name = "txt_Password";
             this.txt_Password.Size = new System.Drawing.Size(318, 22);
-            this.txt_Password.TabIndex = 8;
+            this.txt_Password.TabIndex = 14;
             this.txt_Password.UseSystemPasswordChar = true;
             // 
             // txt_Username
             // 
-            this.txt_Username.Location = new System.Drawing.Point(218, 82);
+            this.txt_Username.Location = new System.Drawing.Point(276, 79);
             this.txt_Username.Name = "txt_Username";
             this.txt_Username.Size = new System.Drawing.Size(318, 22);
-            this.txt_Username.TabIndex = 7;
+            this.txt_Username.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(100, 152);
+            this.label2.Location = new System.Drawing.Point(158, 149);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 16);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 12;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 88);
+            this.label1.Location = new System.Drawing.Point(155, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 16);
-            this.label1.TabIndex = 5;
+            this.label1.TabIndex = 11;
             this.label1.Text = "UserName";
             // 
-            // btnRegister
+            // btnBack
             // 
-            this.btnRegister.Location = new System.Drawing.Point(274, 337);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(116, 34);
-            this.btnRegister.TabIndex = 10;
-            this.btnRegister.Text = "Đăng ký";
-            this.btnRegister.UseVisualStyleBackColor = true;
-            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            this.btnBack.Location = new System.Drawing.Point(309, 374);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(174, 32);
+            this.btnBack.TabIndex = 16;
+            this.btnBack.Text = "Trở lại đăng nhập";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // Dangnhap
+            // DangKy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 390);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnRegister);
-            this.Controls.Add(this.btn_login);
             this.Controls.Add(this.txt_Password);
             this.Controls.Add(this.txt_Username);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Dangnhap";
-            this.Text = "Dangnhap";
+            this.Name = "DangKy";
+            this.Text = "DangKy";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,11 +109,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.TextBox txt_Password;
         private System.Windows.Forms.TextBox txt_Username;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnBack;
     }
 }
--- THBaoMat/Dangnhap.cs
+++ THBaoMat/Dangnhap.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,42 +19,91 @@ namespace THBaoMat
             InitializeComponent();
         }
 
-        bool check_TextBox(string user, string pass)
+        private bool KiemTraTextbox(string user, string pass)
         {
-            if (user == "")
+            if (string.IsNullOrEmpty(user))
             {
-                MessageBox.Show("Chua dien thong tin UserName");
-                txt_Username.Focus();
+                HienThongBao("Chưa điền thông tin UserName", txt_Username);
                 return false;
             }
-            else if (pass == "")
+            if (string.IsNullOrEmpty(pass))
             {
-                MessageBox.Show("Chua dien thong tin Password");
-                txt_Password.Focus();
+                HienThongBao("Chưa điền thông tin Password", txt_Password);
                 return false;
             }
             return true;
         }
 
+        private void HienThongBao(string message, Control control)
+        {
+            MessageBox.Show(message);
+            control.Focus();
+        }
+
         private void btn_login_Click(object sender, EventArgs e)
         {
-            string user = txt_Username.Text;
-            string pass = txt_Password.Text;
-            if (check_TextBox(user, pass))
+            string user = txt_Username.Text.Trim();
+            string pass = txt_Password.Text.Trim();
+
+            if (!KiemTraTextbox(user, pass))
+                return;
+
+            try
             {
                 Database.Set_Database(user, pass);
-                if (Database.Connect())
+                if (!Database.Connect())
                 {
-                    OracleConnection c = Database.Get_Connect();
-                    MessageBox.Show("Dang nhap thanh cong\nServerVersion: " + c.ServerVersion);
-                    HocVien hv = new HocVien();
-                    hv.Show();
+                    MessageBox.Show("Username hoặc mật khẩu không chính xác.");
+                    return;
                 }
-                else
-                {
-                    MessageBox.Show("Dang nhap that bai");
-                }
+                Database.GrantPermissions(user);
+                UpdateAndGetLastLogin(Database.Get_Connect(), user);
+
+                new Main().Show();
+                this.Hide();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}");
+            }
+        }
+
+        //Cập nhật và lấy thời gian đăng nhập
+        public static void UpdateAndGetLastLogin(OracleConnection connection, string username)
+        {
+            try
+            {
+                string sessionID = Guid.NewGuid().ToString();
+
+                string upsertQuery = @"MERGE INTO MANAGER.LOGIN_SESSIONS target USING (SELECT :username AS Username FROM dual) source
+                               ON (target.Username = source.Username)
+                               WHEN MATCHED THEN UPDATE SET LastLogin = SYSTIMESTAMP, SessionID = :sessionID, IS_LOGGED_IN = 1
+                               WHEN NOT MATCHED THEN INSERT (Username, LastLogin, SessionID, IS_LOGGED_IN) 
+                               VALUES (:username, SYSTIMESTAMP, :sessionID, 1)";
+
+                OracleCommand cmd = new OracleCommand(upsertQuery, connection);
+                cmd.Parameters.Add(new OracleParameter(":username", username));
+                cmd.Parameters.Add(new OracleParameter(":sessionID", sessionID));
+                cmd.ExecuteNonQuery(); 
+
+                cmd.CommandText = "SELECT LastLogin FROM MANAGER.LOGIN_SESSIONS WHERE Username = :username";
+                cmd.Parameters.Clear(); 
+                cmd.Parameters.Add(new OracleParameter(":username", username));  
+                object result = cmd.ExecuteScalar(); 
+
+                string lastLogin = result?.ToString() ?? "Không có thông tin đăng nhập trước đó.";
+                MessageBox.Show($"Thời gian đăng nhập lần cuối của tài khoản '{username}': {lastLogin}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            new DangKy().Show();
+            this.Hide();
         }
     }
 }
